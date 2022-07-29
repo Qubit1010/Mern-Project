c1c5658cc228e6170e07278a8e565dad4f26cc82
Qubit 1010 <hassanaleem86@gmail.com>
2022-07-29 08:18:49 -0700

    Goals Controller and Routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getGoal,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+router.route('/').get(getGoal).post(setGoal)
+
+// router.get("/", getGoal);
+// router.post("/", setGoal);
+
+
+router.route('/').put(updateGoal).delete(deleteGoal)
+
+
+// router.put("/:id", updateGoal);
+// router.delete("/:id", deleteGoal);
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello Server")
+const express = require("express");
+const { use } = require("./routes/goalRoutes");
+const dotenv = require("dotenv").config();
+const port = process.env.port || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
+// console.log("Hello World");
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
